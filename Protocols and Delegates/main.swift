8dad73dec2fe81285e2d25ab77e3eb2abc931929
Carlo Contardi <c.contardi@spacecoding.it>
2025-03-01 15:25:11 +0100

    [Feature] add: protocol and delegate pattern

--- Protocols and Delegates/main.swift
+++ Protocols and Delegates/main.swift
@@ -1,11 +1,51 @@
-//
-//  main.swift
-//  Protocols and Delegates
-//
-//  Created by Carlo Contardi on 01/03/25.
-//
+protocol AdvanceLifeSupport{
+    func performCPR()
+}
 
-import Foundation
+class EmergencyCallHandler{
+    var delegate: AdvanceLifeSupport?
+    
+    func assessSituation(){
+        print("Can you tell me what happened?")
+    }
+    func medicalEmergency(){
+        delegate?.performCPR()
+    }
+}
 
-print("Hello, World!")
+struct Paramedic: AdvanceLifeSupport{
+    init(handler: EmergencyCallHandler){
+        handler.delegate = self
+    }
+    func performCPR() {
+        print("The paramedic does chest compression, 30 per second.")
+    }
+}
 
+
+class Doctor: AdvanceLifeSupport{
+    
+    init(handler: EmergencyCallHandler){
+        handler.delegate = self
+    }
+    func performCPR() {
+        print("The doctor does chest compression, 30 per second.")
+    }
+    func useStethescope(){
+        print("Listening for heart sounds.")
+    }
+}
+
+class Surgeon: Doctor{
+    override func performCPR() {
+        print("The surgeon does chest compression, 30 per second.")
+    }
+    func useElectricDrill(){
+        print("Whirr...")
+    }
+}
+let emilio = EmergencyCallHandler()
+let pete = Paramedic(handler: emilio)
+
+emilio.assessSituation()
+emilio.medicalEmergency()
